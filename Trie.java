40ec4c9f25be5cb513e3154d03f7de4cce30f94c
Jnapp18 <jnapp18@gmail.com>
2014-09-30 16:57:15 -0400

    Create Trie.java

---
+++ Trie.java
@@ -0,0 +1,115 @@
+  public class Trie
+    {
+      
+        public final static char ENDOFWORD = '\u0001';
+
+
+        public Trie()
+        {
+            root = new Node('r');
+        }
+        
+        private class Node
+        {
+            public int value;
+            public Node firstChild;
+            public Node nextSibling;
+
+            public Node(int value)
+            {
+                this.value = value;
+                firstChild = null;
+                nextSibling = null;
+            }
+        }
+
+        public void add(String word)
+        {
+            addRE(root,word + ENDOFWORD, 0);
+        }
+
+        private boolean addRE(Node root, String word, int wordIndex)
+        {
+            if (wordIndex == word.length())
+                return false;
+            int c = word.charAt(wordIndex);
+            Node last = null;
+            Node next = root.firstChild;
+
+            //this while loop recuseivly finds the starting point of the word. then parses through the trie to see if the word/ or a prefix of the word, has already been added.
+            while (next != null)
+            {
+                if (next.value < c)
+                {
+                    last = next;
+                    next = next.nextSibling;
+                }
+                else if (next.value == c)
+                {
+                    return addRE(next, word, wordIndex + 1);
+                }
+                else break;
+            }
+            Node node = new Node(c);
+            if (last == null)
+            {
+                root.firstChild = node;
+                node.nextSibling = next;
+            }
+            else
+            {
+                last.nextSibling = node;
+                node.nextSibling = next;
+            }
+            for (int i = wordIndex + 1; i < word.length(); i++)
+            {
+                node.firstChild = new Node(word.charAt(i));
+                node = node.firstChild;
+            }
+            return true;
+        }
+
+        public boolean search(String word)
+        {
+            return searchRE(root, word + ENDOFWORD, 0);
+        }
+
+        private boolean searchRE(Node root, String word, int wordIndex)
+        {
+            if (wordIndex == word.length())
+                return true;
+            int c = word.charAt(wordIndex);
+            Node next = root.firstChild;
+            while (next != null)
+            {
+                if (next.value < c) next = next.nextSibling;
+                else if (next.value == c) return searchRE(next, word, wordIndex + 1);
+                else return false;
+            }
+            return false;
+        }
+        public boolean checkPrefix(String prefix)
+        {
+            return checkPrefixRE(root, prefix, 0);
+        }
+        private boolean checkPrefixRE(Node root, String word, int wordIndex)
+        {
+            if (wordIndex == word.length())
+            {
+                return true;
+            }
+            int c = word.charAt(wordIndex);
+            Node next = root.firstChild;
+            while (next != null)
+            {
+                if (next.value < c) next = next.nextSibling;
+                else if (next.value == c) return checkPrefixRE(next, word, wordIndex + 1);
+                else break;
+            }
+            return false;
+        }   
+
+        private Node root;
+        private int maxDepth;
+        private boolean ignoreCase;
+    }   
