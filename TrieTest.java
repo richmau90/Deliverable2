8a51fba1d5c257e2f440bfa822069aedead325b9
Jnapp18 <jnapp18@gmail.com>
2014-10-01 17:27:50 -0400

    Create TrieTest.java

---
+++ TrieTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+import org.mockito.*;
+
+public class TrieTest {
+
+
+	@Test
+	//Tests
+	
+}
